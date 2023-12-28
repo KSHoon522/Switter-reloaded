45aa435767dd1a02fbc165d8039e505a98a7271e
Sang Hoon Kim <KSHoon522@gamil.com>
2023-12-28 14:30:43 +0900

    Setup

---
+++ src/routes/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuNgqYTC6QImetcKEdAoidO9nOGQs53LA",
+  authDomain: "switter-reloaded.firebaseapp.com",
+  projectId: "switter-reloaded",
+  storageBucket: "switter-reloaded.appspot.com",
+  messagingSenderId: "956965757296",
+  appId: "1:956965757296:web:d8020afa967e8185026fb5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
